d819a9fd484613d6603347a746a5bf966d4b8b5b
Xhofe <Xhofe@users.noreply.github.com>
2022-11-09 10:29:55 +0000

    deploy: alist-org/docs@62825b4f354326996624135427ff8a27d67aaa23

---
+++ assets/manual.html.16aea1be.js
@@ -0,0 +1,39 @@
+import{_ as m}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as u,c as v,a as e,b as l,w as n,d as s,e as b,r as c}from"./app.a39d0c0d.js";const h={},p=e("h1",{id:"\u624B\u52A8\u5B89\u88C5",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u624B\u52A8\u5B89\u88C5","aria-hidden":"true"},"#"),s(" \u624B\u52A8\u5B89\u88C5")],-1),_=e("h3",{id:"\u83B7\u53D6-alist",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u83B7\u53D6-alist","aria-hidden":"true"},"#"),s(" \u83B7\u53D6 AList")],-1),x=s("\u6253\u5F00 "),k={href:"https://github.com/Xhofe/alist/releases",target:"_blank",rel:"noopener noreferrer"},f=s("AList Release"),g=s(" \u4E0B\u8F7D\u5F85\u90E8\u7F72\u7CFB\u7EDF\u5BF9\u5E94\u7684\u6587\u4EF6\u3002\u6700\u65B0\u7248\u7684\u524D\u7AEF\u5DF2\u7ECF\u548C\u540E\u7AEF\u6253\u5305\u597D\u4E86\uFF0C\u4E0D\u7528\u518D\u4E0B\u8F7D\u524D\u7AEF\u6587\u4EF6\u4E86\u3002"),y=e("h3",{id:"\u8FD0\u884C",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u8FD0\u884C","aria-hidden":"true"},"#"),s(" \u8FD0\u884C")],-1),L=e("div",{class:"language-bash ext-sh line-numbers-mode"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# \u89E3\u538B\u4E0B\u8F7D\u7684\u6587\u4EF6\uFF0C\u5F97\u5230\u53EF\u6267\u884C\u6587\u4EF6\uFF1A"),s(`
+`),e("span",{class:"token function"},"tar"),s(),e("span",{class:"token parameter variable"},"-zxvf"),s(` alist-xxxx.tar.gz
+`),e("span",{class:"token comment"},"# \u6388\u4E88\u7A0B\u5E8F\u6267\u884C\u6743\u9650\uFF1A"),s(`
+`),e("span",{class:"token function"},"chmod"),s(` +x alist
+`),e("span",{class:"token comment"},"# \u8FD0\u884C\u7A0B\u5E8F"),s(`
+./alist server
+`),e("span",{class:"token comment"},"# \u83B7\u5F97\u7BA1\u7406\u5458\u4FE1\u606F"),s(`
+./alist admin
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),A=e("div",{class:"language-bash ext-sh line-numbers-mode"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# \u89E3\u538B\u4E0B\u8F7D\u7684\u6587\u4EF6\uFF0C\u5F97\u5230\u53EF\u6267\u884C\u6587\u4EF6\uFF1A"),s(`
+`),e("span",{class:"token function"},"tar"),s(),e("span",{class:"token parameter variable"},"-zxvf"),s(` alist-xxxx.tar.gz
+`),e("span",{class:"token comment"},"# \u6388\u4E88\u7A0B\u5E8F\u6267\u884C\u6743\u9650\uFF1A"),s(`
+`),e("span",{class:"token function"},"chmod"),s(` +x alist
+`),e("span",{class:"token comment"},"# \u8FD0\u884C\u7A0B\u5E8F"),s(`
+./alist server
+`),e("span",{class:"token comment"},"# \u83B7\u5F97\u7BA1\u7406\u5458\u4FE1\u606F"),s(`
+./alist admin
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),z=e("div",{class:"language-bash ext-sh line-numbers-mode"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# \u89E3\u538B\u4E0B\u8F7D\u7684\u6587\u4EF6\uFF0C\u5F97\u5230\u53EF\u6267\u884C\u6587\u4EF6\uFF1A"),s(`
+`),e("span",{class:"token function"},"unzip"),s(` alist-xxxx.zip
+`),e("span",{class:"token comment"},"# \u8FD0\u884C\u7A0B\u5E8F"),s(`
+.`),e("span",{class:"token punctuation"},"\\"),s(`alist.exe server
+`),e("span",{class:"token comment"},"# \u83B7\u5F97\u7BA1\u7406\u5458\u4FE1\u606F"),s(`
+.`),e("span",{class:"token punctuation"},"\\"),s(`alist.exe admin
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),w=e("div",{class:"language-bash ext-sh line-numbers-mode"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# \u5B89\u88C5"),s(`
+scoop `),e("span",{class:"token function"},"install"),s(` alist
+`),e("span",{class:"token comment"},"# \u8FD0\u884C"),s(`
+alist server
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),V=e("p",null,"xxxx \u6307\u7684\u662F\u4E0D\u540C\u7CFB\u7EDF/\u67B6\u6784\u5BF9\u5E94\u7684\u540D\u79F0\uFF0C\u4E00\u822C Linux-x86/64 \u4E3A alist-linux-amd64\u3002\u5982\u679C\u4F60\u7684 glibc \u7248\u672C\u592A\u4F4E\uFF0C\u5EFA\u8BAE\u4E0B\u8F7D musl \u7248\u672C",-1),W=s("\u5F53\u4F60\u770B\u5230 "),B=e("code",null,"start server@0.0.0.0:5244",-1),D=s(" \u7684\u8F93\u51FA\uFF0C\u4E4B\u540E\u6CA1\u6709\u62A5\u9519\uFF0C\u8BF4\u660E\u64CD\u4F5C\u6210\u529F\u3002 \u7B2C\u4E00\u6B21\u8FD0\u884C\u65F6\u4F1A\u8F93\u51FA\u521D\u59CB\u5BC6\u7801\u3002\u7A0B\u5E8F\u9ED8\u8BA4\u76D1\u542C 5244 \u7AEF\u53E3\u3002 \u73B0\u5728\u6253\u5F00 "),E=e("code",null,"http://ip:5244",-1),N=s(" \u53EF\u4EE5\u770B\u5230\u767B\u5F55\u9875\u9762\uFF0CWebDAV \u8BF7\u53C2\u9605 "),R=s("WebDav"),S=s("\u3002"),T=b(`<h3 id="\u5B88\u62A4\u8FDB\u7A0B-linux" tabindex="-1"><a class="header-anchor" href="#\u5B88\u62A4\u8FDB\u7A0B-linux" aria-hidden="true">#</a> \u5B88\u62A4\u8FDB\u7A0B(Linux)</h3><p>\u4F7F\u7528\u4EFB\u610F\u65B9\u5F0F\u7F16\u8F91 <code>/usr/lib/systemd/system/alist.service</code> \u5E76\u6DFB\u52A0\u5982\u4E0B\u5185\u5BB9\uFF0C\u5176\u4E2D path_alist \u4E3A AList \u6240\u5728\u7684\u8DEF\u5F84</p><div class="language-conf ext-conf line-numbers-mode"><pre class="language-conf"><code>[Unit]
+Description=alist
+After=network.target
+ 
+[Service]
+Type=simple
+WorkingDirectory=path_alist
+ExecStart=path_alist/alist server
+Restart=on-failure
+ 
+[Install]
+WantedBy=multi-user.target
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u7136\u540E\uFF0C\u6267\u884C <code>systemctl daemon-reload</code> \u91CD\u8F7D\u914D\u7F6E\uFF0C\u73B0\u5728\u4F60\u53EF\u4EE5\u4F7F\u7528\u8FD9\u4E9B\u547D\u4EE4\u6765\u7BA1\u7406\u7A0B\u5E8F\uFF1A</p><ul><li>\u542F\u52A8: <code>systemctl start alist</code></li><li>\u5173\u95ED: <code>systemctl stop alist</code></li><li>\u914D\u7F6E\u5F00\u673A\u81EA\u542F: <code>systemctl enable alist</code></li><li>\u53D6\u6D88\u5F00\u673A\u81EA\u542F: <code>systemctl disable alist</code></li><li>\u72B6\u6001: <code>systemctl status alist</code></li><li>\u91CD\u542F: <code>systemctl restart alist</code></li></ul>`,5);function I(C,O){const o=c("ExternalLinkIcon"),d=c("Tabs"),r=c("RouterLink");return u(),v("div",null,[p,_,e("p",null,[x,e("a",k,[f,l(o)]),g]),y,l(d,{data:[{title:"Linux"},{title:"macOS"},{title:"Windows"},{title:"win(scoop)"}],"tab-id":"os"},{tab0:n(({title:a,value:i,isActive:t})=>[L]),tab1:n(({title:a,value:i,isActive:t})=>[A]),tab2:n(({title:a,value:i,isActive:t})=>[z]),tab3:n(({title:a,value:i,isActive:t})=>[w]),_:1},8,["data"]),V,e("p",null,[W,B,D,E,N,l(r,{to:"/zh/guide/webdav.html"},{default:n(()=>[R]),_:1}),S]),T])}const j=m(h,[["render",I],["__file","manual.html.vue"]]);export{j as default};
