f2c74d4e8404387db8afba14aabd855899656fef
Xhofe <Xhofe@users.noreply.github.com>
2023-02-14 14:07:49 +0000

    deploy: alist-org/docs@a986a7aa84b95682cc3d025ed41689798344ea1e

--- adb.js
+++ adb.js
@@ -27,7 +27,7 @@ function ABDetected() {
       <img src='https://creatives-1301677708.file.myqcloud.com/images/placeholder/wwads-friendly-ads.png' width='130'>
       </a>
       <div class='wwads-content'>
-      <a href='https://wwads.cn/page/whitelist-wwads' class='wwads-text' target='_blank' rel='nofollow'>为了支持AList的发展，请将我们的网站加入广告拦截器的白名单，感谢您的支持！</a>
+      <a href='https://wwads.cn/page/whitelist-wwads' class='wwads-text' target='_blank' rel='nofollow'>为了支持AList更好的发展，请将我们的网站加入广告拦截器的白名单，感谢您的支持！</a>
       <a href='https://wwads.cn/page/end-user-privacy' class='wwads-poweredby' title='万维广告 ～ 让广告更优雅，且有用' target='_blank'>
       <span>万维</span><span>广告</span>
       </a></div><a class='wwads-hide' onclick='parentNode.remove()' title='隐藏广告'><svg xmlns='http://www.w3.org/2000/svg' width='6' height='7'><path d='M.879.672L3 2.793 5.121.672a.5.5 0 11.707.707L3.708 3.5l2.12 2.121a.5.5 0 11-.707.707l-2.12-2.12-2.122 2.12a.5.5 0 11-.707-.707l2.121-2.12L.172 1.378A.5.5 0 01.879.672z'></path></svg></a>`
@@ -47,5 +47,5 @@ docReady(function () {
     if (window._AdBlockInit === undefined) {
       ABDetected();
     }
-  }, 3000);
+  }, 2000);
 });
